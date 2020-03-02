29c8d837583a7c65009d2b15d0e6774f53aa0515
Mounia El Baz <mouniaelbaz@Mounias-MacBook-Pro.local>
2020-03-02 15:55:48 +0100

    class pysalesforce

--- pysalesforce/PySalesforce.py
+++ pysalesforce/PySalesforce.py
@@ -4,6 +4,9 @@ import json
 import os
 import random
 
+import requests
+
+from pysalesforce.auth import get_access_token
 
 
 class DBStream:
@@ -16,3 +19,20 @@ class DBStream:
         self.ssh_tunnel = None
         self.dbstream_instance_id = 'df-' + datetime.datetime.now().strftime('%s') + '-' + str(
             random.randint(1000, 9999))
+
+
+class pysalesforce:
+
+    def __init__(self):
+        self.client="client"
+        self.objects=[]
+        self.access_token="null"
+
+    def auth(self):
+        self.access_token=get_access_token(self.client)
+
+    def get_objects(self):
+        params = {"Authorization":"Bearer "+self.access_token}
+        url = "https://%s.my.salesforce.com/services/data/v44.0/sobjects/" %self.client
+        result=requests.get(url, params=params).json()
+        self.objects=[r["name"] for r in result.get("sobjects")]
--- pysalesforce/auth.py
+++ pysalesforce/auth.py
@@ -5,9 +5,10 @@ def get_access_token(client):
     params = {
         "grant_type": "password",
         "client_id": os.environ["SALESFORCE_%s_CLIENT_ID" %client],
-        "client_secret": os.environ["SALESFORCE_CLIENT_SECRET"],
-        "username": os.environ["SALESFORCE_USERNAME"],
-        "password": os.environ["SALESFORCE_PASSWORD"] + os.environ["SALESFORCE_SECURITY_TOKEN"],
+        "client_secret": os.environ["SALESFORCE_%s_CLIENT_SECRET"],
+        "username": os.environ["SALESFORCE_%s_USERNAME"],
+        "password": os.environ["SALESFORCE_PASSWORD"] + os.environ["SALESFORCE_%s_SECURITY_TOKEN"],
     }
     url = "https://login.salesforce.com/services/oauth2/token"
-    return requests.post(url, params=params).json()
+    r=requests.post(url, params=params).json()
+    return r.get("access_token")
