868e616cae83aa4b5bcc58149ea65f9cafdb58c5
Mounia El Baz <mouniaelbaz@Mounias-MacBook-Pro.local>
2020-04-20 18:21:05 +0200

    adapted queries to azure

--- pysalesforce/Salesforce.py
+++ pysalesforce/Salesforce.py
@@ -1,4 +1,7 @@
+from datetime import time
+
 import psycopg2
+import pyodbc as pyodbc
 import yaml
 import requests
 from pysalesforce.auth import get_access_token
@@ -55,7 +58,7 @@ class Salesforce:
             'Content-type': 'application/json'
         }
         params = {
-            "q": self.query(object_name.get('api_name'), since)
+            "q": self.query(object_name.get('name'), since)
         }
         url = self.base_url + "/services/data/%s/query/" % self.api_version
         if not next_records_url:
@@ -87,7 +90,7 @@ class Salesforce:
 
     def process_data(self, raw_data):
         object_row = []
-        object_description = self.describe_objects(raw_data.get("object")["api_name"])
+        object_description = self.describe_objects(raw_data.get("object")["name"])
         for r in raw_data["records"]:
             _object = dict()
             for o in object_description:
@@ -95,6 +98,15 @@ class Salesforce:
                     _object[o.lower()] = str(r.get(o))
                 else:
                     _object[o.lower()] = r.get(o)
+            object_row.append(_object)
+        return object_row
+
+    def process_data_endpoint(self, raw_data):
+        object_row = []
+        for r in raw_data:
+            _object = dict()
+            for k in r.keys():
+                _object[k]=r.get(k)
             object_row.append(_object)
         return object_row
 
@@ -106,73 +118,93 @@ class Salesforce:
                     column_list.append(c)
         return column_list
 
-    def create_temp_table(self, o):
+    def create_temp_table(self, table):
         try:
             self.datamart.execute_query(
                 '''drop table if exists %(schema_name)s.%(table_name)s_temp cascade; ''' +
                 '''CREATE TABLE %(schema_name)s.%(table_name)s_temp AS (SELECT * FROM %(schema_name)s.%(table_name)s 
                 where id= 1)''' % {
-                    'table_name': o.get('table'),
+                    'table_name': table,
                     "schema_name": self.schema_prefix})
         except:
-            pass
+            print('temp_table not created')
 
-    def send_temp_data(self, data, o):
+    def send_temp_data(self, data,table):
         column_names = self.get_column_names(data)
         data_to_send = {
             "columns_name": column_names,
             "rows": [[r[c] for c in column_names] for r in data],
-            "table_name": self.schema_prefix + '.' + o.get('table') + '_temp'}
+            "table_name": self.schema_prefix + '.' + table + '_temp'}
+        print('True')
         self.datamart.send_data(
             data=data_to_send,
             replace=False)
 
 
-    def _clean(self, object_name):
+    def _clean(self, table):
         selecting_id = 'id'
         try:
             print('trying to clean')
             cleaning_query = """
                     DELETE FROM %(schema_name)s.%(table_name)s WHERE %(id)s IN (SELECT distinct %(id)s FROM %(schema_name)s.%(table_name)s_temp);
-                    INSERT INTO %(schema_name)s.%(table_name)s (SELECT * FROM %(schema_name)s.%(table_name)s_temp);
+                    INSERT INTO %(schema_name)s.%(table_name)s 
+                    SELECT * FROM %(schema_name)s.%(table_name)s_temp;
                     DELETE FROM %(schema_name)s.%(table_name)s_temp;
-                    """ % {"table_name": object_name.get('table'),
+                    """ % {"table_name": table,
                            "schema_name": self.schema_prefix,
                            "id": selecting_id}
             self.datamart.execute_query(cleaning_query)
-        except psycopg2.errors.UndefinedTable:
+            print('cleaned')
+        except psycopg2.errors.UndefinedTable :
             print('destination table does not exist, will be created')
             insert_query = """
-            DROP TABLE IF EXISTS %(schema_name)s.%(table_name)s CASCADE;
-            CREATE TABLE %(schema_name)s.%(table_name)s as (SELECT * FROM %(schema_name)s.%(table_name)s_temp);
-            """% {"table_name": object_name.get('table'),
+            DROP TABLE IF EXISTS %(schema_name)s.%(table_name)s;
+            CREATE TABLE %(schema_name)s.%(table_name)s as SELECT * FROM %(schema_name)s.%(table_name)s_temp;
+            """% {"table_name": table,
                            "schema_name": self.schema_prefix}
+            self.datamart.execute_query(insert_query)
+        except pyodbc.ProgrammingError:
+            print('destination table does not exist, will be created')
+            insert_query = """
+                        DROP TABLE IF EXISTS %(schema_name)s.%(table_name)s;
+                        SELECT * into %(schema_name)s.%(table_name)s from %(schema_name)s.%(table_name)s_temp;
+                        """ % {"table_name": table,
+                               "schema_name": self.schema_prefix}
             self.datamart.execute_query(insert_query)
 
 
     def main(self, since_start=False, batchsize=100, endpoint=False):
         if endpoint==False:
             for p in self.get_objects():
-                print('Starting ' + p.get('api_name'))
-                self.create_temp_table(p)
+                print('Starting ' + p.get('name'))
+                self.create_temp_table(p.get('table'))
                 since = None
                 if not since_start:
                     since=start_end_from_last_call(self,p)
-                    raw_data = self.execute_query(p, batchsize, since)
-                    next_url = raw_data.get("next_records_url")
+                raw_data = self.execute_query(p, batchsize, since)
+                next_url = raw_data.get("next_records_url")
+                data = self.process_data(raw_data)
+                self.send_temp_data(data, p.get('table'))
+                while next_url:
+                    raw_data = self.execute_query(p, batchsize, next_records_url=next_url, since=None)
+                    next_url=raw_data.get("next_records_url")
                     data = self.process_data(raw_data)
                     self.send_temp_data(data, p)
-                    while next_url:
-                        raw_data = self.execute_query(p, batchsize, next_records_url=next_url, since=None)
-                        next_url=raw_data.get("next_records_url")
-                        data = self.process_data(raw_data)
-                        self.send_temp_data(data, p)
+                print('Ended ' + p.get('name'))
+                self._clean(p.get('table'))
         else:
             for p in self.get_endpoint():
                 print('Starting ' + p.get('name'))
-                self.create_temp_table(p)
+                self.create_temp_table(p.get('table'))
+                since=None
+                if not since_start:
+                    since=time.yesterday()
+                raw_data=self.retrieve_endpoint(p.get('name'),since)
+                data=self.process_data_endpoint(raw_data)
+                print(data)
+                self.send_temp_data(data, p.get('table'))
+            print('Ended ' + p.get('name'))
+            self._clean(p.get('table'))
 
-                    #raw_data=self.
-            self._clean(p)
-            print('Ended ' + p.get('api_name'))
+
 
