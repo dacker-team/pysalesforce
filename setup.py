f873db640386c100c9b103522ba2245b85294324
Geoffrey Chetcuti <gchetcuti-ext@moethennessy.com>
2021-10-13 11:38:42 +0200

    Add prefix_schema

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysalesforce",
-    version="0.0.10",
+    version="0.0.11",
     author="Dacker",
     author_email="hello@dacker.co",
     description="A Salesforce connector",
